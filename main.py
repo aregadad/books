4f851f9b86f7c3e5ad29a5ba5b7fbd3fb070da61
aregadad <biafleks@gmail.com>
2023-05-03 18:13:55 +0300

    rebuild parsing book page function

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from pathvalidate import sanitize_filename
 from urllib.parse import urljoin, urlparse
 
 
-def parse_book_page(book_soup):
+def parse_book_page(book_id):
+    book_page_url = f'https://tululu.org/b{book_id}/'
+    book_response = requests.get(book_page_url, allow_redirects=False)
+    book_response.raise_for_status()
+    check_for_redirect(book_response, f'{book_id}. No book with this ID')
+    book_soup = BeautifulSoup(book_response.text, 'lxml')           
     book_title = book_soup.find('td', class_='ow_px_td').find('h1').text
     book_name, book_author = map(str.strip, book_title.split('::'))
     sanitized_book_name = sanitize_filename(book_name)
@@ -13,9 +18,13 @@ def parse_book_page(book_soup):
     cover_url = urljoin('https://tululu.org/', cover_web_path)
     book_comments = book_soup.find('td', class_='ow_px_td').find_all('span', class_='black')
     book_genres = tuple(map(lambda x: x.text, book_soup.find('span', class_='d_book').find_all('a')))
+    txt_url = f'https://tululu.org/txt.php?id={book_id}'
+
     return {
         'name': sanitized_book_name,
         'author': book_author,
+        'txt_url': txt_url,
+        'page_url': book_page_url,
         'cover_url': cover_url,
         'comments': book_comments,
         'genres': book_genres,
@@ -67,17 +76,10 @@ def main():
     covers_path.mkdir(exist_ok=True)
     comments_path = Path('comments')
     comments_path.mkdir(exist_ok=True)
-    genres_path = Path('genres')
-    genres_path.mkdir(exist_ok=True)
     for book_id in range(1, 11):
         try:
-            book_url = f'https://tululu.org/b{book_id}/'
-            book_response = requests.get(book_url, allow_redirects=False)
-            book_response.raise_for_status()
-            check_for_redirect(book_response, f'{book_id}. No book with this ID')
-            book = parse_book_page(BeautifulSoup(book_response.text, 'lxml'))           
-            txt_url = f'https://tululu.org/txt.php?id={book_id}'
-            download_txt(txt_url, book_id, book['name'], books_path)
+            book = parse_book_page(book_id)
+            download_txt(book['txt_url'], book_id, book['name'], books_path)
             print(f'{book_id}. Downloaded "{book["name"]}"')  
             print(f'Author: {book["author"]}')
             print(f'Genre: {book["genres"]}')
